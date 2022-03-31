71542d4db35f79ccaf48cbdbe5128ff9b80a53aa
Pavibajju <vjpavi249@gmail.com>
2022-03-31 12:36:45 +0530

    first comit

---
+++ src/BookView.jsx
@@ -0,0 +1,85 @@
+import React, { Component } from "react"
+import axios from 'axios';
+import Button from '@material-ui/core/Button';
+export class Viewbooking extends Component {
+
+   constructor(props) {
+      super(props)
+      this.state = {
+         data: []
+      }
+   }
+
+   handleFormSubmit = result => {
+      localStorage.setItem('id', result);
+   }
+
+   componentDidMount() {
+      axios.get('http://localhost:8080/book/list').then(res => {
+         this.setState({ data: res.data });
+      });
+   }
+
+   render() {
+      const deleteUser = async id => {
+         axios.delete('http://localhost:8080/book/delete/' + id).then(res => {
+            if (res.data.status == 1) {
+               alert("Successfully Deleted...!");
+               this.componentDidMount();
+               window.location.href = "/bookView";
+            }
+            else {
+               // alert("Failed to Delete...!");
+               window.location.href = "/bookView";
+            }
+         });
+
+      };
+      const edit = (id) => {
+         localStorage.setItem('id', id);
+         window.location.href = "/bookEdit";
+      }
+      return (
+         <div className="maincontainer">
+            <h1 className="mr-5 ml-5 mt-5">Booking List</h1>
+            <div className="container mb-5 mt-5 text-left" style={{ padding: "20px" }}>
+               <table class="table table-bordered">
+                  <thead>
+                     <tr>
+                        <th> ID</th>
+                        <th>Serial No</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Qty</th>
+                        <th>Image</th>
+
+                        <th>Action</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     {this.state.data.map((result) => {
+                        return (
+
+                           <tr>
+                              <td>{result.id}</td>
+                              <td>{result.serialNumber}</td>
+                              <td>{result.name}</td>
+                              <td>{result.price}</td>
+                              <td>{result.qty}</td>
+                              <td> <img src={result.imageUrl} height="100px" /></td>
+
+                              <td>
+                                 <Button color="secondary" variant="contained" onClick={() => deleteUser(result.id)}>Delete</Button>&nbsp;&nbsp;
+                                 <Button color="primary" variant="contained" onClick={() => edit(result.id)}>Edit</Button></td>
+                           </tr>
+                        )
+                     })}
+                  </tbody>
+               </table>
+            </div>
+         </div>
+      )
+   }
+};
+
+export default Viewbooking;
